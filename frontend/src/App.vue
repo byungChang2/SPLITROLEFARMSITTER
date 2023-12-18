504a9386a2753b25969c0fa2f2ab3229517c3c22
byungChang2 <govlqudckd@gmail.com>
2023-12-18 13:08:43 +0900

    create MainMap.vue,sidevar.vue etc...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,46 @@
 <template>
+  <div id="app">
+    <MainMap/>
+    <SideBar class="side-bar"/>
+  </div>
+</template>
+
+<script>
+import MainMap from '@/components/MainMap'
+import SideBar from '@/components/SideBar'
+
+export default {
+  name: 'App',
+  components: {
+    MainMap,
+    SideBar
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  > .side-bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+  }
+ 
+}
+</style>
+
+
+
+
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -23,4 +65,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
