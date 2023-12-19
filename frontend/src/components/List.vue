48802092f395864a139127c1a4f0360e2b0ea89e
byungChang2 <govlqudckd@gmail.com>
2023-12-19 15:01:59 +0900

    백엔드를타는지테스트

---
+++ frontend/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>List 컴포넌트</h2>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array // 부모 컴포넌트로부터 전달받은 배열
+  },
+  created() {
+    console.log('List 컴포넌트의 items:', this.items);
+  }
+}
+</script>
